4d04ef818550df062b32adea986e8590545ea517
NahedKz <163647941+NahedKz@users.noreply.github.com>
2025-11-06 12:40:14 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,17 @@
 def add(a, b):
     return a + b
-    
+    print("Sum:", add(5, 7))
 
 def sub(a, b):
     return a - b
+print("Sum:", sub(5, 7))
 
-print("Sum:", add(5, 7))
+def div(a, b):
+    return a / b
+print("Sum:", div(5, 7))
+
 def multiply(a, b):
     return a * b
-
 print("Multiply:", multiply(3, 4))
 
 print('Calculator Project by Nahed')
